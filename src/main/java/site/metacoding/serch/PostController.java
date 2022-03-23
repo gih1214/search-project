e531a88e0235b2fcb4159b4acda5dccc03b41d9d
gih1214 <gih1214@hanmail.net>
2022-03-24 03:35:43 +0900

    3. 검색 기능 추가 완료

--- src/main/java/site/metacoding/serch/PostController.java
+++ src/main/java/site/metacoding/serch/PostController.java
@@ -3,6 +3,7 @@ package site.metacoding.serch;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 import site.metacoding.serch.domain.PostRepository;
@@ -14,8 +15,9 @@ public class PostController {
     private final PostRepository postRepository;
 
     @GetMapping("/")
-    public String list(Model model) {
+    public String list(Model model, @RequestParam(defaultValue = "") String keyword) {
         model.addAttribute("lists", postRepository.findAll());
+        model.addAttribute("lists", postRepository.mSearch(keyword));
         return "list";
     }
 }
--- src/main/java/site/metacoding/serch/domain/PostRepository.java
+++ src/main/java/site/metacoding/serch/domain/PostRepository.java
@@ -1,7 +1,13 @@
 package site.metacoding.serch.domain;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<BoardTbl, Integer> {
 
+    @Query(value = "SELECT * FROM boardTbl WHERE  title like %:keyword%", nativeQuery = true)
+    List<BoardTbl> mSearch(@Param("keyword") String keyword);
 }
